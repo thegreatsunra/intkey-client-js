c810071fb3b202eb2d4344e3674d92e127cbb7cc
Dane Petersen <thegreatsunra@gmail.com>
2018-02-26 17:01:16 -0600

    Add init module that creates public/private keys

---
+++ init.js
@@ -0,0 +1,19 @@
+const { createContext, CryptoFactory } = require('sawtooth-sdk/signing')
+const fs = require('fs')
+const path = require('path')
+
+const env = require('./env')
+const context = createContext('secp256k1')
+const privateKey = context.newRandomPrivateKey()
+const signer = new CryptoFactory(context).newSigner(privateKey)
+
+const output = `PRIVATE_KEY=${privateKey.asHex()}\nPUBLIC_KEY=${signer.getPublicKey().asHex()}\nVALIDATOR_URL=http://localhost:8008`
+
+fs.writeFile(path.resolve(__dirname, './.env'), output, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+})
+
+console.log('\nGenerated .env file with public/private keys\n')
+console.log(output, '\n')
